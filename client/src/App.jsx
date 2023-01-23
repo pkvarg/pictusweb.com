de100435392b760651fee8a9931bab3f127d4b00
Peter Varga <pkvarg@yahoo.se>
2023-01-23 11:45:14 +0100

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { logo } from './assets'
 import { Home, CreatePost } from './pages'
+import { Footer } from './components'
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path='/create-post' element={<CreatePost />} />
         </Routes>
       </main>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h1>Credits: </h1>
+      <a href='https://openai.com/' target='_blank'>
+        Open AI
+      </a>
+      <a href='https://www.jsmastery.pro/' target='_blank'>
+        Javascript Mastery
+      </a>
+      <a className='developer' href='https://pictusweb.sk/en' target='_blank'>
+        &#60;&#47;&#62; by PICTUSWEB Development
+      </a>
+    </div>
+  )
+}
+
+export default Footer
